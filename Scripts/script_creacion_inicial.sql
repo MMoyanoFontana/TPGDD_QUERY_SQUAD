21a046055d6bac11a909dfa57c5b44fb34bcf161
Maximiliano Moyano <mmoyanofontana@frba.utn.edu.ar>
2023-06-23 22:27:03 -0300

    Agrego carga de categorias a script de creacion inicial

--- Scripts/script_creacion_inicial.sql
+++ Scripts/script_creacion_inicial.sql
@@ -964,3 +964,32 @@ PRINT('MIGRANDO ENVIOS DE MENSAJERIA')
 Exec QUERY_SQUAD.Migracion_Envio_Mensajeria
 PRINT('MIGRANDO CUPONES POR PEDIDO')
 Exec QUERY_SQUAD.Migracion_Pedido_Cupones
+
+PRINT('CARGANDO CATEGORIAS DE LOCAL')
+INSERT INTO QUERY_SQUAD.Categoria_Local (categoria_local_tipo_local_id, categoria_local_categoria)
+VALUES 
+ (1, 'parrilla'),
+ (1, 'heladeria'),
+ (1, 'comida rapida'),
+ (2, 'minimercado'),
+ (2, 'supermercado')
+
+UPDATE QUERY_SQUAD.Local
+SET local_categoria_local_id = 1
+WHERE local_tipo_local_id = 1 AND local_id < 60
+
+UPDATE QUERY_SQUAD.Local
+SET local_categoria_local_id = 2
+WHERE local_tipo_local_id = 1 AND local_id BETWEEN 60 AND 120
+
+UPDATE QUERY_SQUAD.Local
+SET local_categoria_local_id = 3
+WHERE local_tipo_local_id = 1 AND local_id > 120
+
+UPDATE QUERY_SQUAD.Local
+SET local_categoria_local_id = 4
+WHERE local_tipo_local_id = 2 AND local_id < 350
+
+UPDATE QUERY_SQUAD.Local
+SET local_categoria_local_id = 5
+WHERE local_tipo_local_id = 2 AND local_id >= 350
